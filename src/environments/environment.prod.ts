6b793879791123b419e3ab0465d79687d7a44235
Habibulla <habi79972@gmail.com>
2026-02-20 15:13:14 +0530

    changes in github actions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-    apiUrl: 'http://localhost:8080/',
+   // apiUrl: 'http://localhost:8080/',
   //apiUrl: 'http://AiResumeAnalyser-env.eba-p29rpqpm.ap-south-1.elasticbeanstalk.com/',
+  apiUrl: 'https://13.204.215.154/',
   appName: 'AI Resume Screener (Prod)'
 };
