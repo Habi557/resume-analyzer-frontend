d9796c7e6f830f55f1defb29ab1b76d55dab3231
Habibulla <habi79972@gmail.com>
2026-02-20 15:53:09 +0530

    testing cicd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/',
+  //apiUrl: 'http://localhost:8080/',
   //apiUrl: 'http://AiResumeAnalyser-env.eba-p29rpqpm.ap-south-1.elasticbeanstalk.com/',
+    apiUrl: 'https://13.204.215.154:8080/',
+
 
   appName: 'AI Resume Screener (Dev)'
 };
