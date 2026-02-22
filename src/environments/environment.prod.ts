660da9aedec7e3be1d1204bb892582151c340882
Habibulla <habi79972@gmail.com>
2026-02-22 10:30:34 +0530

    changing https to http in enviroment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://13.204.215.154:8080/',
+  apiUrl: 'http://13.204.215.154:8080/',
   appName: 'AI Resume Screener (Prod)'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:8080/',
+  apiUrl: 'http://localhost:8080/',
   //apiUrl: 'http://AiResumeAnalyser-env.eba-p29rpqpm.ap-south-1.elasticbeanstalk.com/',
-    apiUrl: 'https://13.204.215.154:8080/',
+   // apiUrl: 'https://13.204.215.154:8080/',
 
 
   appName: 'AI Resume Screener (Dev)'
